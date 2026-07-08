6ee8e94b4b1392a8ab809451050af4a89b66a4b9
Ishit02422 <22bmiit022@gmail.com>
2026-07-09 00:48:52 +0530

    update please

--- resume_screening/frontend/src/index.js
+++ resume_screening/frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
 import App from "./App";
 import { AuthProvider } from './context/AuthContext';
+import axios from 'axios';
+
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = 'https://ai-based-resume-screening-system-b208.onrender.com';
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
